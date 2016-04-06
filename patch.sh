0836d86aa0ddeb2b4bc0c594db0c6a76b334133f
Rumkin <dev@rumk.in>
2016-04-06 10:40:41 +0000

    lift up patch source

--- patch.sh
+++ patch.sh
@@ -20,36 +20,7 @@ function remove_profile {
     sed -i '/source .profile.d\/\*.sh/d' $1
 }
 
-# # Apply or remove patch into directory
-# function patch {
-#     ACTION=$1
-#     if [ $# -gt 1 ]; then
-#         DIR=$2
-#     else
-#         DIR=$PWD
-#     fi
-#
-#     PROFILE=$DIR/.profile
-#     PROFILE_D=$DIR/.profile.d
-#
-#     . $(dirname $0)/patch_profile.sh
-#
-#     case $ACTION in
-#         install)
-#             add_profile $PROFILE
-#             add_profile_dir $PROFILE_D
-#         ;;
-#         uninstall)
-#             remove_profile $PROFILE
-#             echo "Remove .profile.d directory if it not neccessary!" >&2
-#         ;;
-#         *)
-#             echo 'Unknown action' >&2
-#             exit 1
-#         ;;
-#     esac
-# }
-
+# Patch profile file and create profile.d directory
 function patch_install {
     PROFILE=$1
 
@@ -57,6 +28,7 @@ function patch_install {
     create_profile_dir $(dirname $PROFILE)/.profile.d
 }
 
+# Patch uninstall
 function patch_uninstall {
     if [ $# -lt 1 ]; then
         if [ -z "$PROFILE" ]; then
@@ -69,6 +41,7 @@ function patch_uninstall {
     remove_profile $PROFILE
 }
 
+# Detect os home skeleton directory
 function skel_location {
     local BASENAME=$1
 
